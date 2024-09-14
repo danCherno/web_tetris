1e8a2fd405b330733f38774ee22ce0500c2d737f
Cherno <daniel.cherno9@gmail.com>
2024-09-14 14:04:26 +0300

    added block and its constructor

--- tetris/index.ts
+++ tetris/index.ts
@@ -4,6 +4,36 @@ let score: number = 0;
 // console.log(gameGrid); // testing
 // clear line function (row index) slice the row out and create a new row of zeros at the end
 // gameGrid[5] = Array(10).fill('1'); //testing
+
+class Block
+{
+  // properties
+  color: string;  // color of the block
+  isImportant: boolean; // whether the block is checked for isBlockUnder
+  id: string; // unique id for the block (used a lot for grid)
+  blockElement: HTMLElement; // the block element in the DOM
+  posInPiece: string; // position of the block in the piece (A-D)
+
+  constructor(color: string, posInPiece: string, isImportant: boolean) 
+  {
+    // assignments
+    this.color = color;
+    this.isImportant = isImportant;
+    this.id = `b${posInPiece}-${crypto.randomUUID()}`; // example: "bC-36b8f84d-df4e-4d49-b662-bcde71a8764f"
+    this.posInPiece = posInPiece;
+    this.blockElement = document.createElement('div');
+
+
+    // block element setup
+    this.blockElement.className = `block`;
+    this.blockElement.id = this.id;
+    this.blockElement.style.backgroundColor = this.color;
+    document.getElementById('gameLayout')!.appendChild(this.blockElement); // adding block to the game layout (fyi, ! is for ignoring null values)
+    
+
+  }
+}
+
 function clearLine(row: number): void {
   gameGrid.splice(row, 1);
   gameGrid.push(Array(10).fill('1'));
