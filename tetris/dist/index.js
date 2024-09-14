f4891d38368976bbfde3749ba58e0e6b4f252064
raamhumphries <raamhomfris2006@gmail.com>
2024-09-15 00:33:37 +0300

    changing gamegrid aray
    make it show the cell cordinants insted of '0'

--- tetris/dist/index.js
+++ tetris/dist/index.js
@@ -1,5 +1,7 @@
 // array of zeros
-var gameGrid = Array.from({ length: 22 }, function () { return Array(10).fill('0'); });
+var gameGrid = Array.from({ length: 22 }, function (_, rowIndex) {
+    return Array.from({ length: 10 }, function (_, colIndex) { return rowIndex + "," + colIndex; });
+});
 var score = 0;
 // console.log(gameGrid); // testing
 // clear line function (row index) slice the row out and create a new row of zeros at the end
@@ -11,13 +13,13 @@ var Block = /** @class */ (function () {
         this.isImportant = isImportant;
         this.id = "b" + posInPiece + "-" + crypto.randomUUID(); // example: "bC-36b8f84d-df4e-4d49-b662-bcde71a8764f"
         this.posInPiece = posInPiece;
-        this.blockElement = document.createElement('div');
+        this.blockElement = document.createElement("div");
         this.position = [5, 22]; //center top of the game grid
         // block element setup
         this.blockElement.className = "block";
         this.blockElement.id = this.id;
         this.blockElement.style.backgroundColor = this.color;
-        document.getElementById('gameLayout').appendChild(this.blockElement); // adding block to the game layout (fyi, ! is for ignoring null values)
+        document.getElementById("gameLayout").appendChild(this.blockElement); // adding block to the game layout (fyi, ! is for ignoring null values)
         // getters
         function getColor() {
             return this.color;
@@ -39,19 +41,19 @@ var Block = /** @class */ (function () {
         }
         // methods
         function fall() {
-            gameGrid[this.position[0]][this.position[1]] = '0';
+            gameGrid[this.position[0]][this.position[1]] = "0";
             this.position[1]--;
             gameGrid[this.position[0]][this.position[1]] = this.id;
             //todo: update for css grid
         }
         function right() {
-            gameGrid[this.position[0]][this.position[1]] = '0';
+            gameGrid[this.position[0]][this.position[1]] = "0";
             this.position[0]--;
             gameGrid[this.position[0]][this.position[1]] = this.id;
             //todo: update for css grid
         }
         function left() {
-            gameGrid[this.position[0]][this.position[1]] = '0';
+            gameGrid[this.position[0]][this.position[1]] = "0";
             this.position[0]++;
             gameGrid[this.position[0]][this.position[1]] = this.id;
             //todo: update for css grid
@@ -59,7 +61,7 @@ var Block = /** @class */ (function () {
         function isBlockUnder() {
             if (!isImportant)
                 return false;
-            if (gameGrid[this.position[0] - 1][this.position[1]] !== '0')
+            if (gameGrid[this.position[0] - 1][this.position[1]] !== "0")
                 return true;
             return false;
         }
@@ -68,22 +70,22 @@ var Block = /** @class */ (function () {
 }());
 function clearLine(row) {
     gameGrid.splice(row, 1);
-    gameGrid.push(Array(10).fill('1'));
+    gameGrid.push(Array(10).fill("1"));
     score++;
 }
 function renderGridToHTML(grid) {
-    var html = '<table>';
+    var html = "<table>";
     grid.forEach(function (row) {
-        html += '<tr>';
+        html += "<tr>";
         row.forEach(function (cell) {
             html += "<td>" + cell + "</td>";
         });
-        html += '</tr>';
+        html += "</tr>";
     });
-    html += '</table>';
+    html += "</table>";
     return html;
 }
 console.log(gameGrid);
-document.getElementById('gameLayout').innerHTML = renderGridToHTML(gameGrid);
+document.getElementById("gameLayout").innerHTML = renderGridToHTML(gameGrid);
 // clearLine(5); //testing
 // render main
--- tetris/index.ts
+++ tetris/index.ts
@@ -1,102 +1,91 @@
 // array of zeros
-const gameGrid: string[][] = Array.from({ length: 22 }, () => Array(10).fill('0'));
+const gameGrid: string[][] = Array.from({ length: 22 }, (_, rowIndex) =>
+  Array.from({ length: 10 }, (_, colIndex) => `${rowIndex},${colIndex}`)
+);
 let score: number = 0;
 // console.log(gameGrid); // testing
 // clear line function (row index) slice the row out and create a new row of zeros at the end
 // gameGrid[5] = Array(10).fill('1'); //testing
 
-class Block
-{
+class Block {
   // properties
-  private color: string;  // color of the block
+  private color: string; // color of the block
   private isImportant: boolean; // whether the block is checked for isBlockUnder
   private id: string; // unique id for the block (used a lot for grid)
   private blockElement: HTMLElement; // the block element in the DOM
   private posInPiece: string; // position of the block in the piece (A-D)
   private position: [number, number]; // position of the block in the game grid (x, y)
 
-  constructor(color: string, posInPiece: string, isImportant: boolean) 
-  {
+  constructor(color: string, posInPiece: string, isImportant: boolean) {
     // assignments
     this.color = color;
     this.isImportant = isImportant;
     this.id = `b${posInPiece}-${crypto.randomUUID()}`; // example: "bC-36b8f84d-df4e-4d49-b662-bcde71a8764f"
     this.posInPiece = posInPiece;
-    this.blockElement = document.createElement('div');
+    this.blockElement = document.createElement("div");
     this.position = [5, 22]; //center top of the game grid
-
 
     // block element setup
     this.blockElement.className = `block`;
     this.blockElement.id = this.id;
     this.blockElement.style.backgroundColor = this.color;
-    document.getElementById('gameLayout')!.appendChild(this.blockElement); // adding block to the game layout (fyi, ! is for ignoring null values)
-    
+    document.getElementById("gameLayout")!.appendChild(this.blockElement); // adding block to the game layout (fyi, ! is for ignoring null values)
+
     // getters
 
-    function getColor(): string
-    {
+    function getColor(): string {
       return this.color;
     }
 
-    function getIsImportant(): boolean
-    {
+    function getIsImportant(): boolean {
       return this.isImportant;
     }
 
-    function getId(): string
-    {
+    function getId(): string {
       return this.id;
     }
 
-    function getBlockElement(): HTMLElement
-    {
+    function getBlockElement(): HTMLElement {
       return this.blockElement;
     }
 
-    function getPosition(): [number, number]
-    {
+    function getPosition(): [number, number] {
       return this.position;
     }
 
-    function getPositionInPiece(): string
-    {
+    function getPositionInPiece(): string {
       return this.posInPiece;
     }
 
     // methods
 
-    function fall(): void
-    {
-      gameGrid[this.position[0]][this.position[1]] = '0';
+    function fall(): void {
+      gameGrid[this.position[0]][this.position[1]] = "0";
       this.position[1]--;
       gameGrid[this.position[0]][this.position[1]] = this.id;
 
       //todo: update for css grid
     }
 
-    function right(): void
-    {
-      gameGrid[this.position[0]][this.position[1]] = '0';
+    function right(): void {
+      gameGrid[this.position[0]][this.position[1]] = "0";
       this.position[0]--;
       gameGrid[this.position[0]][this.position[1]] = this.id;
 
       //todo: update for css grid
     }
 
-    function left(): void
-    {
-      gameGrid[this.position[0]][this.position[1]] = '0';
+    function left(): void {
+      gameGrid[this.position[0]][this.position[1]] = "0";
       this.position[0]++;
       gameGrid[this.position[0]][this.position[1]] = this.id;
 
       //todo: update for css grid
     }
 
-    function isBlockUnder(): boolean
-    {
+    function isBlockUnder(): boolean {
       if (!isImportant) return false;
-      if (gameGrid[this.position[0] - 1][this.position[1]] !== '0') return true;
+      if (gameGrid[this.position[0] - 1][this.position[1]] !== "0") return true;
       return false;
     }
   }
@@ -104,32 +93,25 @@ class Block
 
 function clearLine(row: number): void {
   gameGrid.splice(row, 1);
-  gameGrid.push(Array(10).fill('1'));
+  gameGrid.push(Array(10).fill("1"));
   score++;
 }
 function renderGridToHTML(grid: string[][]): string {
-  let html = '<table>';
-  grid.forEach(row => {
-    html += '<tr>';
-    row.forEach(cell => {
+  let html = "<table>";
+  grid.forEach((row) => {
+    html += "<tr>";
+    row.forEach((cell) => {
       html += `<td>${cell}</td>`;
     });
-    html += '</tr>';
+    html += "</tr>";
   });
-  html += '</table>';
+  html += "</table>";
   return html;
 }
 console.log(gameGrid);
 
-document.getElementById('gameLayout')!.innerHTML = renderGridToHTML(gameGrid);
+document.getElementById("gameLayout")!.innerHTML = renderGridToHTML(gameGrid);
 
 // clearLine(5); //testing
 
 // render main
-
-
-
-
-
-
-
